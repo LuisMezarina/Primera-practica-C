bd23bdf3a860f67976227f5d8e6e16aad073fba1
LuisMezarina <diegomezarina.provincias@gmail.com>
2026-01-16 00:46:10 -0500

    Primer ejercicio resuelto

--- hello.c
+++ hello.c
@@ -1,1 +1,14 @@
 #include<stdio.h>
+#include<stdbool.h>
+
+int main()
+{
+    int a,b,suma=0;
+    printf("Ingrese valor 1\n");
+    scanf("%d", &a);
+    printf("Ingrese valor 2\n");
+    scanf("%d", &b);
+    suma=a+b;
+    printf("la suma es: %d\n", suma);
+    return 0;
+}

08a5920b7fcff13419168c80e0fcaa76447a3c98
LuisMezarina <diegomezarina.provincias@gmail.com>
2026-01-31 13:03:46 -0500

    Ejercicio 008

---
+++ chatgtp007.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    int numero=0;
+    for (numero=0; numero<=10; numero++)
+    {
+        printf("%d", numero);
+        printf("\n");
+    }
+    return 0;
+    
+}
---
+++ chatgtp008.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    int numero=0;
+    while (numero<=10)
+    {
+        printf("%d", numero);
+        printf("\n");
+        numero++;
+    }
+    return 0;
+
+}
